4fea125a534cc2b6452950a7850cd6be91324374
xn16h77 <32337979+xn16h77@users.noreply.github.com>
2017-10-26 00:34:00 +0900

    Update ss.py

--- modules/ss.py
+++ modules/ss.py
@@ -6,7 +6,6 @@ import win32api
 
 hdesktop = win32gui.GetDesktopWindow()
 
-# size
 width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
 height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
 left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
@@ -15,7 +14,6 @@ top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
 desktop_dc = win32gui.GetWindowDC(hdesktop)
 img_dc = win32ui.CreateDCFromHandle(desktop_dc)
 
-
 mem_dc = img_dc.CreateCompatibleDC()
 
 screenshot = win32ui.CreateBitmap()
@@ -23,8 +21,9 @@ screenshot.CreateCompatibleBitmap(img_dc, width, height)
 mem_dc.SelectObject(screenshot)
 
 mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+
 screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
+
 
 mem_dc.DeleteDC()
 win32gui.DeleteObject(screenshot.GetHandle())
-
