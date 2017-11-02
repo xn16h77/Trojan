a9a7dbfb9e9fe6881d8081a16a8a14f546e33d00
xn16h77 <32337979+xn16h77@users.noreply.github.com>
2017-11-02 13:12:46 +0900

    Update ss.py

--- modules/ss.py
+++ modules/ss.py
@@ -4,23 +4,29 @@ import win32ui
 import win32con
 import win32api
 
+
 hdesktop = win32gui.GetDesktopWindow()
+
 
 width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
 height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
 left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
 top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
 
+
 desktop_dc = win32gui.GetWindowDC(hdesktop)
 img_dc = win32ui.CreateDCFromHandle(desktop_dc)
 
+
 mem_dc = img_dc.CreateCompatibleDC()
+
 
 screenshot = win32ui.CreateBitmap()
 screenshot.CreateCompatibleBitmap(img_dc, width, height)
 mem_dc.SelectObject(screenshot)
 
 mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+
 
 screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
 
